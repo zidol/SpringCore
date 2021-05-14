1280e11f23bfb5e0990739dcc05478a36822a746
zidol <ajh3166@naver.com>
2021-05-14 23:26:51 +0900

    @Configuration

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -25,16 +25,19 @@ public class AppConfig {
     @Bean //스프링 컨테이너에 등록
     //생성자 주입
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("cal AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     //생성자 주입
     public OrderService orderService () {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
