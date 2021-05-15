9864302dfc54d47b58c75697084d4e25c2a2ab8d
zidol <ajh3166@naver.com>
2021-05-15 23:32:51 +0900

    scan 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",// 탐색할 패키지의 시작 위치 지정 , default = @ComponentScan이 붙은 클래스의 패키지 부터
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)//AppConfig.java 및 test 하면서 생성한  것들 제외 시키기 위해.
 )
 public class AutoAppConfig {
