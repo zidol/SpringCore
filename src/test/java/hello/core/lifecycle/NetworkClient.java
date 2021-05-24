fc5d17199b0f5cd24df6ed82d0e6bf573d8894b3
jiHyeokAhn <ajh3166@naver.com>
2021-05-25 00:51:30 +0900

    빈 등록 초기화, 소멸 메서드

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,11 +3,11 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
-
+//public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
-    public NetworkClient () {
+    public NetworkClient() {
         System.out.println("생성자 호출 , url = " + url);
     }
 
@@ -23,21 +23,34 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     public void call(String message) {
         System.out.println("call : " + url + " message = " + message);
     }
+
     //서비스 종료시
     public void disconnect() {
         System.out.println("close : " + url);
     }
 
-    @Override//의존관계 주입이 끝나고 실행
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+//    @Override//의존관계 주입이 끝나고 실행
+//    public void afterPropertiesSet() throws Exception {
+//        System.out.println("NetworkClient.afterPropertiesSet");
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//
+//    @Override
+//    public void destroy() throws Exception {
+//        System.out.println("NetworkClient.destroy");
+//        disconnect();
+//    }
+
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
